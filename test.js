fd8cabd5058e683a4b3dec6cbdc712f360e7e62a
Vamsi8074 <gundlurimanikanta142@gmail.com>
2024-10-06 12:51:27 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+const name = "mani";
+console.log(name);
+// console.log(global);
+// setTimeout(() => {
+//   console.log("Timeout:::");
+//   clearInterval(int);
+// }, 3000);
+// const int = setInterval(() => {
+//   console.log("Interval:::");
+// }, 1000);
+console.log(__dirname);
+console.log(__filename);
