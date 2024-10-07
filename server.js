df95b0f8958014c342d822b9c64cca780b2e39cf
Vamsi8074 <gundlurimanikanta142@gmail.com>
2024-10-07 11:20:19 +0530

    installed packages like lodash,nodemon using NPM

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
+
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  //lodash package
+  const num = _.random(0, 20);
+  console.log(num);
+  const greet = _.once(() => {
+    console.log("greet");
+  });
+
+  greet();
+  greet();
+
   //setting response headers
   /* res.setHeader("Content-Type", "text/plain");
   res.write("hello from backend");
@@ -39,3 +52,6 @@ const server = http.createServer((req, res) => {
 server.listen(3000, "localhost", () => {
   console.log("listening for requests on pot 3000");
 });
+
+//  npm init -y
+//  the above command is used to create a package.json file with default settings.
